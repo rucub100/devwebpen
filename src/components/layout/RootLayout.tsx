ca7d130255bdfd6db224061a11ca6c94a44d84b6
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-01-05 19:09:23 +0100

    feat(layout): implement RootLayout with CSS Grid for responsive design
    #2

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,7 +1,26 @@
+import styles from "./RootLayout.module.css";
+
 export default function RootLayout() {
   return (
-    <div className="min-h-screen h-screen max-h-screen flex flex-col scroll-smooth antialiased dark:bg-neutral-900 dark:text-neutral-200">
-      TODO
+    <div
+      className={`${styles.rootLayout} scroll-smooth antialiased dark:bg-neutral-900 dark:text-neutral-200`}
+    >
+      <nav className={`${styles.navAside} border-r border-neutral-700`}>
+        nav
+      </nav>
+      <header className={`${styles.leftAside} border-r border-neutral-700`}>
+        left aside
+      </header>
+      <main className={`${styles.main}`}>main</main>
+      <aside className={`${styles.rightAside} border-l border-neutral-700`}>
+        right aside
+      </aside>
+      <aside className={`${styles.bottomAside} border-t border-neutral-700`}>
+        bottom aside
+      </aside>
+      <footer className={`${styles.footer} border-t border-neutral-700`}>
+        Status bar
+      </footer>
     </div>
   );
 }
