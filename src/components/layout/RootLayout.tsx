9c12f42aa3f576a192319a5cba0a61eb8ba98f17
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-01-05 22:18:21 +0100

    feat(layout): add resizable left split panel to RootLayout
    #2

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,17 +1,63 @@
+import { useCallback, useState } from "react";
 import styles from "./RootLayout.module.css";
 
 export default function RootLayout() {
+  const [leftSplitIsResizing, setLeftSplitIsResizing] = useState(false);
+  const [leftAsideWidth, setLeftAsideWidth] = useState(200);
+
+  const leftSplitMouseDownHandler = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      event.preventDefault();
+      document.body.style.cursor = "e-resize";
+      setLeftSplitIsResizing(true);
+
+      const mouseUpHandler = (event: MouseEvent) => {
+        event.preventDefault();
+        document.removeEventListener("mousemove", mouseMoveHandler);
+        document.removeEventListener("mouseup", mouseUpHandler);
+        document.body.style.cursor = "auto";
+        setLeftSplitIsResizing(false);
+      };
+
+      const mouseMoveHandler = (event: MouseEvent) => {
+        event.preventDefault();
+        setLeftAsideWidth((prevWidth) => {
+          const newWidth = prevWidth + event.movementX;
+          return newWidth;
+        });
+      };
+
+      document.addEventListener("mouseup", mouseUpHandler);
+      document.addEventListener("mousemove", mouseMoveHandler);
+
+      return () => {
+        document.removeEventListener("mouseup", mouseUpHandler);
+        document.removeEventListener("mousemove", mouseMoveHandler);
+      };
+    },
+    []
+  );
+
   return (
     <div
-      className={`${styles.rootLayout} scroll-smooth antialiased dark:bg-neutral-900 dark:text-neutral-200`}
+      className={`${styles.rootLayout} scroll-smooth antialiased select-none dark:bg-neutral-900 dark:text-neutral-200`}
     >
       <nav className={`${styles.navAside} border-r border-neutral-700`}>
         nav
       </nav>
-      <header className={`${styles.leftAside} border-r border-neutral-700`}>
+      <header
+        className={`${styles.leftAside} border-r border-neutral-700 min-w-max max-w-full`}
+        style={{ width: leftAsideWidth }}
+      >
         left aside
       </header>
-      <main className={`${styles.main}`}>main</main>
+      <div
+        className={`${styles.leftSplit} ${
+          leftSplitIsResizing ? "bg-[--color-primary-500]" : "bg-transparent"
+        }  hover:bg-[--color-primary-500] transition-colors duration-200 delay-300`}
+        onMouseDown={leftSplitMouseDownHandler}
+      ></div>
+      <main className={`${styles.main} min-w-[200px] max-w-max`}>main</main>
       <aside className={`${styles.rightAside} border-l border-neutral-700`}>
         right aside
       </aside>
