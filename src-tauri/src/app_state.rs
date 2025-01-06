46f9a6a22880a24de8e78fd881483338de7d03bd
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-01-06 21:39:36 +0100

    feat(state): implement application state management with DaemonState, Session, and Project structs
    resolves #8

---
+++ src-tauri/src/app_state.rs
@@ -0,0 +1,77 @@
+use std::sync::{
+    atomic::{AtomicU64, Ordering},
+    Mutex,
+};
+
+pub enum DaemonState {
+    Stopped,
+    Starting,
+    Connecting,
+    Running,
+    Error(String),
+}
+
+impl Default for DaemonState {
+    fn default() -> Self {
+        DaemonState::Stopped
+    }
+}
+
+pub struct Session {
+    pub id: u64,
+}
+
+impl Session {
+    fn next_id() -> u64 {
+        static COUNTER: AtomicU64 = AtomicU64::new(0);
+        COUNTER.fetch_add(1, Ordering::SeqCst)
+    }
+}
+
+impl Default for Session {
+    fn default() -> Self {
+        Session {
+            id: Session::next_id(),
+        }
+    }
+}
+
+pub struct Project {
+    pub id: u64,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub path: String,
+    pub session: Session,
+}
+
+impl Project {
+    fn next_id() -> u64 {
+        static COUNTER: AtomicU64 = AtomicU64::new(0);
+        COUNTER.fetch_add(1, Ordering::SeqCst)
+    }
+}
+
+impl Default for Project {
+    fn default() -> Self {
+        Project {
+            id: Project::next_id(),
+            name: None,
+            description: None,
+            path: String::default(),
+            session: Session::default(),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct ViewState {}
+
+#[derive(Default)]
+pub struct AppStateInner {
+    pub daemon_state: DaemonState,
+    pub ephemeral_session: Option<Session>,
+    pub projects: Vec<Project>,
+    pub view_state: ViewState,
+}
+
+pub type AppState = Mutex<AppStateInner>;
