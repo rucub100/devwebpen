6d8bb3214316095be4e3ee0bfae620a8b85c1891
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-01-27 08:04:32 +0100

    feat(debugger): implement retry logic for socket connection in DebugClient
    closes #22

--- src-daemon-debugger/src/main/java/de/curbanov/devwebpen/debugger/DebugClient.java
+++ src-daemon-debugger/src/main/java/de/curbanov/devwebpen/debugger/DebugClient.java
@@ -6,6 +6,8 @@ import java.net.Socket;
 public class DebugClient {
     private final int port;
     private Socket socket;
+    private final int MAX_RETRIES = 20;
+    private final int RETRY_DELAY_MS = 5000;
 
     private final Thread transferInputThread = new Thread(() -> {
         try {
@@ -36,7 +38,27 @@ public class DebugClient {
     }
 
     public void start() throws IOException {
-        socket = new Socket("localhost", port);
+        int retryCount = 0;
+        while (retryCount < MAX_RETRIES) {
+            try {
+                socket = new Socket("localhost", port);
+                break;
+            } catch (IOException e) {
+                System.err.println("Failed to connect to the server. Retrying in " + RETRY_DELAY_MS + "ms...");
+                try {
+                    Thread.sleep(RETRY_DELAY_MS);
+                } catch (InterruptedException e1) {
+                    // ignore
+                } finally {
+                    retryCount++;
+                }
+            }
+        }
+
+        if (socket == null) {
+            throw new IOException("Failed to connect to the server after " + MAX_RETRIES + " retries");
+        }
+
         transferInputThread.start();
         transferOutputThread.start();
     }
