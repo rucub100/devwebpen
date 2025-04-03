cad42c61977db22e2f35ba4120ecffe943d5552e
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-04-03 11:33:14 +0200

    chore: add copyright notices and license information to multiple files

--- scripts/fake-proxy.js
+++ scripts/fake-proxy.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Ruslan Curbanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * This script creates a simple TCP server using Node.js that acts as a fake proxy.
  * It is used to test and debug client connections to a proxy server.
